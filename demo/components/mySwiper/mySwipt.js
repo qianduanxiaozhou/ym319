e5d46ad44e975bf08e96c13b2e008db685769111
qianduanxiaozhou <101966970+qianduanxiaozhou@users.noreply.github.com>
2022-03-20 16:20:10 +0800

    Add files via upload
    猿码前端考核小程序

---
+++ demo/components/mySwiper/mySwipt.js
@@ -0,0 +1,26 @@
+// components/mySwiper/mySwipt.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        imagesUrl: {
+            type: Array,
+            value: []
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
---
+++ demo/pages/index/index.js
@@ -0,0 +1,14 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+      urlList:[
+        "../../image/one.webp",
+        "../../image/two.webp",
+        "../../image/three.webp",
+        "../../image/four.webp"
+      ]
+  }
+})
